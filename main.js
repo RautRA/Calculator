bc256fd209cd4d99ba5681548834773bbcf12f5a
RautRA <168980384+RautRA@users.noreply.github.com>
2024-07-04 20:08:38 +0530

    create basic calculator UI in HTML & hook JS with it

--- main.js
+++ main.js
@@ -22,3 +22,21 @@ console.log("Add 2 + 3: ", operate(2, 3, add));
 console.log("Subtract 3 - 2: ", operate(3, 2, subtract));
 console.log("Multiply 2 * 3: ", operate(2, 3, multiply));
 console.log("Divide 4 / 2: ", operate(4, 2, divide));
+
+const buttons = document.querySelectorAll(".btn");
+const display = document.querySelector("#display");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.value === "calculate") {
+            return
+        } else if (e.target.value === "clear") {
+            display.textContent = "";
+        } else {
+            if (display.textContent === "0") {
+                display.textContent = "";
+            }
+            display.textContent += e.target.value;
+        }
+    });
+});
